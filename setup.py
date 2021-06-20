0ba7e0591d41c060ab6f6432d2eb323614432abb
XuHao <xuhao1@users.noreply.github.com>
2021-06-21 04:12:09 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyAircraftIden",
+    version="1.0",
+    author="xuhao3e8",
+    author_email="xuhao3e8@gmail.com",
+    description="An open-source aircraft identification library in python",
+
+    url="https://www.xuhao1.me/open-source/", 
+
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages(),
+        install_requires=['scipy', "numpy", "matplotlib", "control", "sympy"],
+    exclude_package_data={
+        "data"
+    }
+
+)
